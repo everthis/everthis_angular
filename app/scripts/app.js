7fd4f1fc7750b387e336cfb82b4be2b7d5669d61
everthis <wwwaap@gmail.com>
2014-12-17 15:23:27 +0800

    change angular templateUrl rules for posts.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,21 +24,8 @@ everthisApp.config(['$routeProvider',
         templateUrl: 'partials/posts-list.html',
         controller: 'PhoneListCtrl'
       }).
-      // when('/#/views/:user_id', {
-      //   templateUrl: function($routeParams){ console.log('/#/views/' + $routeParams.user_id + '.html');return '/#/views/' + $routeParams.user_id + '.html'; },
-      //   controller: 'PhoneDetailCtrl'
-      // }).
-
-      when('/views/Bash scripts', {
-        templateUrl: 'views/bash.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      when('/views/Get Parameters from your Script Tag', {
-        templateUrl: 'views/script.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      when('/views/Tutorial Git', {
-        templateUrl: 'views/Git guide.html',
+      when('/views/:postName', {
+        templateUrl: function($routeParams){ return '/views/' + $routeParams.postName + '.html'; },
         controller: 'PhoneDetailCtrl'
       }).
       otherwise({
