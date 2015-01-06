8e0392dec7445e64fbe2b2dc46b69521292e4686
everthis <wwwaap@gmail.com>
2015-01-06 13:32:00 +0800

    unique filter addded.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -26,7 +26,7 @@ everthisControllers.controller('everthisFilm', ['$scope', '$routeParams', 'Film'
     // $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
     //   $scope.mainImageUrl = phone.images[0];
     // });
-    
+
     $scope.film = Film.get({filmId: $routeParams.filmId}, function(film) {
 
     });
@@ -36,3 +36,49 @@ everthisControllers.controller('everthisFilm', ['$scope', '$routeParams', 'Film'
       $scope.mainImageUrl = imageUrl;
     };
   }]);
+
+/**
+ * Filters out all duplicate items from an array by checking the specified key
+ * @param [key] {string} the name of the attribute of each object to compare for uniqueness
+ if the key is empty, the entire object will be compared
+ if the key === false then no filtering will be performed
+ * @return {array}
+ */
+everthisControllers.filter('unique', function () {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+});
