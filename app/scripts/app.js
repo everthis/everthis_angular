979ca1c226b251aaf9a75e96bd4d3f2d277abea1
everthis <wwwaap@gmail.com>
2015-01-28 16:10:13 +0800

    google analytics added.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,61 +27,61 @@ everthisApp.config(['$routeProvider',
         }).
         when('/views/Linux/:postName', {
             templateUrl: function(params) {
-                return '/views/Linux/'+ params.postName + '.html';
+                return '/views/Linux/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/HTML&CSS/:postName', {
             templateUrl: function(params) {
-                return '/views/HTML&CSS/'+ params.postName + '.html';
+                return '/views/HTML&CSS/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/WebDev/:postName', {
             templateUrl: function(params) {
-                return '/views/WebDev/'+ params.postName + '.html';
+                return '/views/WebDev/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/JavaScript/:postName', {
             templateUrl: function(params) {
-                return '/views/JavaScript/'+ params.postName + '.html';
+                return '/views/JavaScript/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Git/:postName', {
             templateUrl: function(params) {
-                return '/views/Git/'+ params.postName + '.html';
+                return '/views/Git/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Workflow/:postName', {
             templateUrl: function(params) {
-                return '/views/Workflow/'+ params.postName + '.html';
+                return '/views/Workflow/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Games/:postName', {
             templateUrl: function(params) {
-                return '/views/Games/'+ params.postName + '.html';
+                return '/views/Games/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Hardware/:postName', {
             templateUrl: function(params) {
-                return '/views/Hardware/'+ params.postName + '.html';
+                return '/views/Hardware/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Algorithms/:postName', {
             templateUrl: function(params) {
-                return '/views/Algorithms/'+ params.postName + '.html';
+                return '/views/Algorithms/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
         when('/views/Others/:postName', {
             templateUrl: function(params) {
-                return '/views/Others/'+ params.postName + '.html';
+                return '/views/Others/' + params.postName + '.html';
             },
             controller: 'everthisCommonCtrl'
         }).
@@ -110,30 +110,29 @@ everthisApp.config(['$provide', function($provide) {
 }]);
 
 /*global angular */
-
+/**
+ * let script tag come into effect in ng-view
+ * @param  {[type]} ) {             return {    restrict: 'E',    scope: false,    link: function(scope, elem, attr)    {      if (attr.type [description]
+ * @return {[type]}   [description]
+ */
 everthisApp.directive('script', function() {
-  return {
-    restrict: 'E',
-    scope: false,
-    link: function(scope, elem, attr)
-    {
-      if (attr.type==='text/javascript-lazy')
-      {
-        var s = document.createElement("script");
-        s.type = "text/javascript";
-        var src = elem.attr('src');
-        if(src!==undefined)
-        {
-            s.src = src;
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function(scope, elem, attr) {
+            if (attr.type === 'text/javascript-lazy') {
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                var src = elem.attr('src');
+                if (src !== undefined) {
+                    s.src = src;
+                } else {
+                    var code = elem.text();
+                    s.text = code;
+                }
+                document.head.appendChild(s);
+                elem.remove();
+            }
         }
-        else
-        {
-            var code = elem.text();
-            s.text = code;
-        }
-        document.head.appendChild(s);
-        elem.remove();
-      }
-    }
-  };
+    };
 });
