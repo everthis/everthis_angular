615940a8557a0a6c545a60968eae13be0d3fe9c6
everthis <wwwaap@gmail.com>
2015-02-09 13:05:23 +0800

    matrix header added.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -125,25 +125,29 @@ everthisControllers.controller('ExampleController', ['$scope', '$interval',
         // $scope.blood_2 = 120;
 
         var stop;
+
         $scope.fight = function() {
             // Don't start a new fight if we are already fighting
             if (angular.isDefined(stop)) return;
-                var width = document.getElementById('q').style.width = window.innerWidth;
-                document.getElementById('q').style.height = '7em';
-                var height = document.getElementById('q').offsetWidth;
-                var letters = Array(256).join(1).split('');
-                var text, x_pos, y_pos;
-                var draw = function() {
-                    q.getContext('2d').fillStyle = 'rgba(0,0,0,.05)';
-                    q.getContext('2d').fillRect(0, 0, width, height);
-                    letters.map(function(y_pos, index) {
-                        q.getContext('2d').fillStyle = '#0F0';
-                        text = String.fromCharCode(48 + Math.random() * 43);
-                        x_pos = index * 10;
-                        q.getContext('2d').fillText(text, x_pos, y_pos);
-                        letters[index] = (y_pos > 158 + Math.random() * 1e4) ? 0 : y_pos + 10;
-                    });
-                };
+            var matrix_rain = document.getElementById('matrix_rain');
+            var matrix_rain_container = document.getElementById("matrix_rain_container");
+            var letters = Array(256).join('1').split('');
+            var text, x_pos, width, height;
+            matrix_rain.style.height = matrix_rain_container.offsetHeight + 'px';
+            matrix_rain.style.width = matrix_rain_container.offsetWidth + 'px';
+            width = matrix_rain.width = matrix_rain_container.offsetWidth;
+            height = matrix_rain.height = matrix_rain_container.offsetHeight;
+            var draw = function() {
+                matrix_rain.getContext('2d').fillStyle = 'rgba(0,0,0,.05)';
+                matrix_rain.getContext('2d').fillRect(0, 0, width, height);
+                letters.map(function(y_pos, index) {
+                    matrix_rain.getContext('2d').fillStyle = '#0F0';
+                    text = String.fromCharCode(48 + Math.random() * 43);
+                    x_pos = index * 10;
+                    matrix_rain.getContext('2d').fillText(text, x_pos, y_pos);
+                    letters[index] = (y_pos > height + Math.random() * 1e4) ? 0 : y_pos + 10;
+                });
+            };
 
             stop = $interval(draw, 55);
         };
