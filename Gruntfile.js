e5706658570d64c17006f8e68719fd304c18fc20
everthis <wwwaap@gmail.com>
2015-03-02 10:07:01 +0800

    page modified.

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,9 +75,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 80,
+        port: 8080,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
