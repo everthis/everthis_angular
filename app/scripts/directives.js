e8ef98890429545b3c4922317de0c937074e86cc
everthis <wwwaap@gmail.com>
2015-04-25 20:41:31 +0800

    contents updated.

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,23 +1,2 @@
 'use strict';
-
 /* Directives */
-
-/**
- *
- *
- * var arrs = [],
-per_item,
-srcc,
-prev_src;
-function ge() {
- per_item = $("#bigImg").trigger('click');
-srcc = $(per_item).attr('src');
-prev_src = srcc;
-if(prev_src !== srcc) {
-	  arrs.push(srcc);
-	prev_src = srcc;
-}
-
-}
-var timer = setInterval(ge, 1000);
- */
